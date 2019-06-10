00fb6cd5836dbf65210a6b64ea505e1d66daa56a
bipink11 <bipinonekumar@gmail.com>
2019-06-10 21:47:55 +0530

    change in file

--- src/AvatarList.js
+++ src/AvatarList.js
@@ -7,6 +7,7 @@ const AvatarList = (props) =>{
         <img src={`https://joeschmoe.io/api/v1/${props.name}`} alt="Avatar"/>
         <h1 className ="">{props.name}</h1>
         <p>{props.work}</p>
+        
     </div>
     )
 }
